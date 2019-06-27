841bd672e7c9b4f415877db0cfe88b661c614331
Akhil <akhilaug2003@gmail.com>
2019-06-27 07:03:19 +0530

    Some minor changes

--- fun/fun.py
+++ fun/fun.py
@@ -170,6 +170,7 @@ class Fun(Cog):
         
     @commands.command()
     async def say(self,ctx,* ,message):
+        """Make the bot say something"""
         msg = escape(message,mass_mentions=True)
         await ctx.send(msg)
 
