eebe3f6f3c0731b6f647f1c3fe0e608c072d42ff
DAzVise <52792999+DAzVise@users.noreply.github.com>
2019-08-09 15:33:48 +0300

    Update fun.py

--- fun/fun.py
+++ fun/fun.py
@@ -269,9 +269,9 @@ class Fun(Cog):
     async def cringe(ctx,* ,message):
         text_list = list(message) #convert string to list to be able to edit it
         for i in range(0,len(message)):
-                if i % 2 == 0:
+            if i % 2 == 0:
                 text_list[i]= text_list[i].lower()
-                else:
+            else:
                 text_list[i]=text_list[i].upper()
         message ="".join(text_list) #convert list back to string(message) to print it as a word
         await ctx.send(message)
