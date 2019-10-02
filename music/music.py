3da606522b6ad7f02816d1217ffddaf85e8fb04f
ThE KinG <43880032+TheKinG2149@users.noreply.github.com>
2019-10-02 19:58:59 +0530

    Update music.py

--- music/music.py
+++ music/music.py
@@ -520,8 +520,7 @@ class Music(commands.Cog):
 
     async def do_stop(self, ctx):
         player = self.bot.wavelink.get_player(ctx.guild.id, cls=Player)
-        song_list = player.entries
-        song_list = []    
+        await player.queue.clear()
         await player.destroy_controller()
         await player.disconnect()
         
