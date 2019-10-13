06c4a7e1aa3d6a4174638af5243e974c3d104cbe
Akhil <akhilaug2003@gmail.com>
2019-10-13 17:42:00 +0530

    indentation oof

--- music/music.py
+++ music/music.py
@@ -159,7 +159,7 @@ class Music(commands.Cog):
         self.bot = bot
         self.players = {}
         if not discord.opus.is_loaded():
-    discord.opus.load_opus('libopus.so')
+            discord.opus.load_opus('libopus.so')
     async def cleanup(self, guild):
         try:
             await guild.voice_client.disconnect()
