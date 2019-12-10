09cb5e36e60f1659750d7b19cb6a59951d30dff4
Akhil <akhilaug2003@gmail.com>
2019-12-10 13:19:28 +0530

    fixes

--- reactrole/reactrole.py
+++ reactrole/reactrole.py
@@ -41,9 +41,9 @@ class ReactionRole(commands.Cog):
         await ctx.send(f'{emoji}-{role} added to that message')
         
     
-    @commands.Cog.listener('on_raw_reaction_add')
+    
     @commands.Cog.listener('on_raw_reaction_remove')
-    async def reactrole_handler(self, payload):
+    async def remove_reactrole_handler(self, payload):
         if payload.guild_id is None:
             return
             
@@ -73,7 +73,7 @@ class ReactionRole(commands.Cog):
             if role.id in data['blacklist']:
                 blacklisted = True
                 return
-        action = member.add_roles if payload.event_type == "REACTION_ADD" else member.remove_roles
+        action = member.remove_roles
         if data['locked']:
             action = None
         if blacklisted:
@@ -81,10 +81,73 @@ class ReactionRole(commands.Cog):
         if allowed:
             action = None                
         if data['drop']:
-            if action == member.add_roles or role not in roles:
+            if role not in roles:
                 action = None
+        if data['verify']:            
+            action = None
+        linked_roles = []
+        if data['limit'] != None:
+            limit = int(data['limit'])
+            cursor = await self.db.find({'message_id':str(payload.message_id)})
+            async for doc in cursor:
+                linked_roles.append(doc['role'])
+            num = len(linked_roles)
+            if limit > num:
+                return
+            else:
+                common = 0
+                for role in roles:
+                    if str(role.id) in linked_roles:
+                        common +=1
+                if common > limit:
+                    action=None
+        if action != None:
+            if reversed:
+                action = member.add_roles
+            await action(role)
+                        
+    @commands.Cog.listener('on_raw_reaction_add')                
+    async def add_reactrole_handler(self, payload):
+        if payload.guild_id is None:
+            return
+            
+        guild = self.bot.get_guild(payload.guild_id)
+        member = guild.get_member(payload.user_id)
+        if member.bot:
+            return
+        data = await self.db.find_one(
+        {
+        'guild_id':str(payload.guild_id),
+        'msg_id':str(payload.message_id),
+        'emoji':str(payload.emoji)
+        })
+        if not data:
+            return
+        role_id = data["role"]
+        role = guild.get_role(role_id)
+        roles = member.roles
+        blacklisted = False
+        allowed = True if data['whitelist'] == [] else False
+        if not allowed:
+            for role in roles:
+                if role.id in data['whitelist']:
+                    allowed = True
+                    return
+        for role in roles:
+            if role.id in data['blacklist']:
+                blacklisted = True
+                return
+        action = member.add_roles 
+        if data['locked']:
+            action = None
+        if blacklisted:
+            action = None
+        if allowed:
+            action = None                
+        if data['drop']:            
+            action = None
         if data['verify']:
-            if action == member.remove_roles or role in roles:
+            if role in roles:
                 action = None
         linked_roles = []
         if data['limit'] != None:
@@ -104,11 +167,8 @@ class ReactionRole(commands.Cog):
                     action=None
         if action != None:
             if reversed:
-                action = member.remove_roles if action == member.add_roles else member.add_roles
-            await action(role)
-                        
-                
-            
+                action = member.remove_roles
+            await action(role)           
         
         
             
