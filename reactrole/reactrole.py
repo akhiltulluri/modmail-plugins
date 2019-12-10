6dfc8ad650b2e616f8734b631a97298a9e6b8d7e
Akhil <akhilaug2003@gmail.com>
2019-12-10 17:21:05 +0530

    hmm

--- reactrole/reactrole.py
+++ reactrole/reactrole.py
@@ -98,8 +98,8 @@ class ReactionRole(commands.Cog):
                 if rol.id in data['whitelist']:
                     allowed = True
                     return
-        for rol in roles:
-            if str(rol.id) in data['blacklist']:
+        for rol1 in roles:
+            if str(rol1.id) in data['blacklist']:
                 blacklisted = True
                 return
         action = member.remove_roles
